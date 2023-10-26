0c798ae8f78c4e8c30cb0ee0a7efe49e8d415d6a
lace04 <luisace04@gmail.com>
2023-10-26 12:15:39 -0500

    second commit

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,7 +3,7 @@ function RoutedProtected() {
     <div>
       <h1>Dashboard Protected page</h1>
       <iframe
-        src={process.env.NODE_RED_DASHBOARD_URL}
+        src={process.env.NODE_RED_DASHBOARD_URL?.replace(/^http:/, 'https:')}
         style={{ width: '150%', height: '100vh' }}
       />
     </div>
